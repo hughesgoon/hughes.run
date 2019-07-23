a408841c5a55baf50a86f4556cbd4d78da65610f
Hyeonjae.kim <45625362+hughesgoon@users.noreply.github.com>
2019-07-23 22:39:41 +0900

    env: Set metadata for my own blog

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,29 +1,29 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: `Run! Hughes`,
+  description: `미숙한 학생 개발자의 런타임 메모리. 공부한 내용, 공유하고싶은 글 여러 잡다한 이야기를 나눕니다.`,
+  author: `Hughes (Hyeonjae Kim)`,
+  introduction: `열심히 달리는중인 미숙한 학생개발자. 인공지능에 관심이 많습니다. (최근엔 프론트엔드도!)`,
+  siteUrl: `https://hughes.run`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `hughesgoon`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    facebook: `guswo0527`, // Your Facebook account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
-  keywords: [`blog`],
+  icon: `content/assets/favicon.png`, // Add your favicon
+  keywords: [`blog`, 'frontend', 'student'],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'hughesgoon/hughes.run', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'hughes',
   },
   share: {
-    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+    facebookAppId: '659071141234460', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'UA-144366745-1', // Add your google analytics tranking ID
 }
